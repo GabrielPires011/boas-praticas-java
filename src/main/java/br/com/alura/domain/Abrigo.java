fb7342c82ad98b41b9a759178c34a31c560b3998
GabrielPires011 <pires.rocha011@gmail.com>
2024-07-29 13:33:34 -0300

    add AbrigoServiceTest deveVerificarSeDispararRequisicaoGetSeraChamado

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -67,4 +67,10 @@ public class Abrigo {
         this.email = email;
     }
 
+    @Override
+    public String toString() {
+        return """
+                "id":%s,"nome":"%s","telefone":"%s","email":"%s"
+                """.formatted(this.id, this.nome, this.telefone, this.email);
+    }
 }
---
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -0,0 +1,47 @@
+package br.com.alura.service;
+
+import br.com.alura.client.HttpService;
+import br.com.alura.domain.Abrigo;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AbrigoServiceTest {
+    private final HttpService client = mock(HttpService.class);
+    private final AbrigoService abrigoService = new AbrigoService(client);
+    private final HttpResponse<String> response = mock(HttpResponse.class);
+    private final Abrigo abrigo = new Abrigo("Teste", "6181880392", "abrigo_alura@gmail.com");
+
+    @Test
+    public void deveVerificarSeDispararRequisicaoGetSeraChamado() throws IOException, InterruptedException {
+        abrigo.setId(0L);
+        abrigo.setNome("Teste");
+
+        String expectedAbrigosCadastrados = "Abrigos cadastrados:";
+        String expectedIdENome = "0 - Teste";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[{\"id\":0,\"nome\":\"Teste\"}]");
+        when(client.disparaRequisicao(anyString(), isNull(), anyString(), isNull())).thenReturn(response);
+
+        abrigoService.listarAbrigo();
+
+        System.setOut(System.out);
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        assertEquals(expectedAbrigosCadastrados, lines[0].trim());
+        assertEquals(expectedIdENome, lines[1].trim());
+    }
+}
